d0ae72aef5620645335cfe75432efd4ea8a1eaf0
manager <christopher.ferreira.silva@outlook.com>
2019-05-22 22:20:09 -0300

    Aplicado a Toolbar

--- app/src/main/java/br/com/etecia/appestrela/CadastroActivity.java
+++ app/src/main/java/br/com/etecia/appestrela/CadastroActivity.java
@@ -1,7 +1,10 @@
 package br.com.etecia.appestrela;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.idToolbarCadastro);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("App Name");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+
+        super.onBackPressed();
     }
 }
--- app/src/main/java/br/com/etecia/appestrela/LoginActivity.java
+++ app/src/main/java/br/com/etecia/appestrela/LoginActivity.java
@@ -1,7 +1,10 @@
 package br.com.etecia.appestrela;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.idToolbarLogin);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Sign in");
+    }
+
+    public void btnLogar(View view) {
+        Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
